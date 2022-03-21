eeba8926f82c5dfbc9146cacd936cc892808b238
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-03-22 01:47:25 +0200

    added changes

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Setup Server
-const port = 5000; // port number
+//const port = 5000; // port number
 //listen to 
-app.listen(port, listening);
+app.listen();
 
-function listening() {
-    console.log(`server is runing on localhost:${port}`);
-}
+// function listening() {
+//     console.log(`server is runing on localhost:${port}`);
+// }
 
 //GET route that returns the projectData object
 app.get('/all', getData);
